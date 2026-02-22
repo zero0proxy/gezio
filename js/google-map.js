dc8d35f721022255ac5a98b11dfa2e073ae8ab92
zero0proxy <batsikadzejoni@gmail.com>
2026-02-22 18:36:38 +0400

    feat: setup EmailJS booking logic, add custom map and update UI styles

--- js/google-map.js
+++ js/google-map.js
@@ -1,62 +1,40 @@
-
 var google;
 
-function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
-    // -8.224454
-    
+function initMap() {
+    // 1. Координаты твоей локации (Тбилиси, Мариам Цалкаманидзе)
+    // Если точка будет немного не там, ты можешь легко изменить эти цифры
+    var myLocation = new google.maps.LatLng(41.72187210215934, 44.897606310439414);
+
+    // 2. Настройки карты
     var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 7,
-
-        // The latitude and longitude to center the map (always required)
-        center: myLatlng,
-
-        // How you would like to style the map. 
-        scrollwheel: false,
+        zoom: 15, // Масштаб (можешь сделать 14 для отдаления или 16 для приближения)
+        center: myLocation,
+        scrollwheel: false, // Отключает случайный зум при прокрутке страницы колесиком
+        mapTypeControl: false,
+        streetViewControl: false, // Убираем лишнего человечка с карты
         styles: [
-            {
-                "featureType": "administrative.country",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "visibility": "simplified"
-                    },
-                    {
-                        "hue": "#ff0000"
-                    }
-                ]
-            }
+            // Делаем карту чуть более минималистичной, чтобы подходила под дизайн
+            { featureType: "administrative", elementType: "labels.text.fill", stylers: [{ color: "#444444" }] },
+            { featureType: "landscape", elementType: "all", stylers: [{ color: "#f2f2f2" }] },
+            { featureType: "poi", elementType: "all", stylers: [{ visibility: "off" }] },
+            { featureType: "road", elementType: "all", stylers: [{ saturation: -100 }, { lightness: 45 }] },
+            { featureType: "transit", elementType: "all", stylers: [{ visibility: "off" }] }
         ]
     };
 
-    
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
+    // 3. Создаем саму карту в блоке с id="map"
     var mapElement = document.getElementById('map');
-
-    // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
-    
-    var addresses = ['New York'];
 
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
-            });
-
-        });
-    }
-    
+    // 4. Добавляем маркер на нашу точку
+    var marker = new google.maps.Marker({
+        position: myLocation,
+        map: map,
+        title: 'GeziO Office'
+        // Если хочешь использовать свою иконку маркера (раскомментируй строку ниже)
+        // icon: 'images/loc.png' 
+    });
 }
-google.maps.event.addDomListener(window, 'load', init);
+
+// Запускаем инициализацию при загрузке страницы
+google.maps.event.addDomListener(window, 'load', initMap);
--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
- AOS.init({
- 	duration: 800,
- 	easing: 'slide'
- });
+AOS.init({
+	duration: 800,
+	easing: 'slide'
+});
 
-(function($) {
+(function ($) {
 
 	"use strict";
 
 	var isMobile = {
-		Android: function() {
+		Android: function () {
 			return navigator.userAgent.match(/Android/i);
 		},
-			BlackBerry: function() {
+		BlackBerry: function () {
 			return navigator.userAgent.match(/BlackBerry/i);
 		},
-			iOS: function() {
+		iOS: function () {
 			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
 		},
-			Opera: function() {
+		Opera: function () {
 			return navigator.userAgent.match(/Opera Mini/i);
 		},
-			Windows: function() {
+		Windows: function () {
 			return navigator.userAgent.match(/IEMobile/i);
 		},
-			any: function() {
+		any: function () {
 			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
 		}
 	};
 
 
 	$(window).stellar({
-    responsive: true,
-    parallaxBackgrounds: true,
-    parallaxElements: true,
-    horizontalScrolling: false,
-    hideDistantElements: false,
-    scrollProperty: 'scroll'
-  });
+		responsive: true,
+		parallaxBackgrounds: true,
+		parallaxElements: true,
+		horizontalScrolling: false,
+		hideDistantElements: false,
+		scrollProperty: 'scroll'
+	});
 
 
-	var fullHeight = function() {
+	var fullHeight = function () {
 
 		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
+		$(window).resize(function () {
 			$('.js-fullheight').css('height', $(window).height());
 		});
 
@@ -50,9 +50,9 @@
 	fullHeight();
 
 	// loader
-	var loader = function() {
-		setTimeout(function() { 
-			if($('#ftco-loader').length > 0) {
+	var loader = function () {
+		setTimeout(function () {
+			if ($('#ftco-loader').length > 0) {
 				$('#ftco-loader').removeClass('show');
 			}
 		}, 1);
@@ -60,26 +60,26 @@
 	loader();
 
 	// Scrollax
-   $.Scrollax();
+	$.Scrollax();
 
-	var carousel = function() {
+	var carousel = function () {
 		$('.carousel-car').owlCarousel({
 			center: true,
 			loop: true,
 			autoplay: true,
-			items:1,
+			items: 1,
 			margin: 30,
 			stagePadding: 0,
 			nav: false,
 			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
-			responsive:{
-				0:{
+			responsive: {
+				0: {
 					items: 1
 				},
-				600:{
+				600: {
 					items: 2
 				},
-				1000:{
+				1000: {
 					items: 3
 				}
 			}
@@ -87,19 +87,19 @@
 		$('.carousel-testimony').owlCarousel({
 			center: true,
 			loop: true,
-			items:1,
+			items: 1,
 			margin: 30,
 			stagePadding: 0,
 			nav: false,
 			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
-			responsive:{
-				0:{
+			responsive: {
+				0: {
 					items: 1
 				},
-				600:{
+				600: {
 					items: 2
 				},
-				1000:{
+				1000: {
 					items: 3
 				}
 			}
@@ -108,7 +108,7 @@
 	};
 	carousel();
 
-	$('nav .dropdown').hover(function(){
+	$('nav .dropdown').hover(function () {
 		var $this = $(this);
 		// 	 timer;
 		// clearTimeout(timer);
@@ -116,55 +116,55 @@
 		$this.find('> a').attr('aria-expanded', true);
 		// $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
 		$this.find('.dropdown-menu').addClass('show');
-	}, function(){
+	}, function () {
 		var $this = $(this);
-			// timer;
+		// timer;
 		// timer = setTimeout(function(){
-			$this.removeClass('show');
-			$this.find('> a').attr('aria-expanded', false);
-			// $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
-			$this.find('.dropdown-menu').removeClass('show');
+		$this.removeClass('show');
+		$this.find('> a').attr('aria-expanded', false);
+		// $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
+		$this.find('.dropdown-menu').removeClass('show');
 		// }, 100);
 	});
 
 
 	$('#dropdown04').on('show.bs.dropdown', function () {
-	  console.log('show');
+		console.log('show');
 	});
 
 	// scroll
-	var scrollWindow = function() {
-		$(window).scroll(function(){
+	var scrollWindow = function () {
+		$(window).scroll(function () {
 			var $w = $(this),
-					st = $w.scrollTop(),
-					navbar = $('.ftco_navbar'),
-					sd = $('.js-scroll-wrap');
+				st = $w.scrollTop(),
+				navbar = $('.ftco_navbar'),
+				sd = $('.js-scroll-wrap');
 
 			if (st > 150) {
-				if ( !navbar.hasClass('scrolled') ) {
-					navbar.addClass('scrolled');	
+				if (!navbar.hasClass('scrolled')) {
+					navbar.addClass('scrolled');
 				}
-			} 
+			}
 			if (st < 150) {
-				if ( navbar.hasClass('scrolled') ) {
+				if (navbar.hasClass('scrolled')) {
 					navbar.removeClass('scrolled sleep');
 				}
-			} 
-			if ( st > 350 ) {
-				if ( !navbar.hasClass('awake') ) {
-					navbar.addClass('awake');	
+			}
+			if (st > 350) {
+				if (!navbar.hasClass('awake')) {
+					navbar.addClass('awake');
 				}
-				
-				if(sd.length > 0) {
+
+				if (sd.length > 0) {
 					sd.addClass('sleep');
 				}
 			}
-			if ( st < 350 ) {
-				if ( navbar.hasClass('awake') ) {
+			if (st < 350) {
+				if (navbar.hasClass('awake')) {
 					navbar.removeClass('awake');
 					navbar.addClass('sleep');
 				}
-				if(sd.length > 0) {
+				if (sd.length > 0) {
 					sd.removeClass('sleep');
 				}
 			}
@@ -173,110 +173,110 @@
 	scrollWindow();
 
 	var isMobile = {
-		Android: function() {
+		Android: function () {
 			return navigator.userAgent.match(/Android/i);
 		},
-			BlackBerry: function() {
+		BlackBerry: function () {
 			return navigator.userAgent.match(/BlackBerry/i);
 		},
-			iOS: function() {
+		iOS: function () {
 			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
 		},
-			Opera: function() {
+		Opera: function () {
 			return navigator.userAgent.match(/Opera Mini/i);
 		},
-			Windows: function() {
+		Windows: function () {
 			return navigator.userAgent.match(/IEMobile/i);
 		},
-			any: function() {
+		any: function () {
 			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
 		}
 	};
 
-	var counter = function() {
-		
-		$('#section-counter, .hero-wrap, .ftco-counter').waypoint( function( direction ) {
+	var counter = function () {
 
-			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
+		$('#section-counter, .hero-wrap, .ftco-counter').waypoint(function (direction) {
+
+			if (direction === 'down' && !$(this.element).hasClass('ftco-animated')) {
 
 				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
-				$('.number').each(function(){
+				$('.number').each(function () {
 					var $this = $(this),
 						num = $this.data('number');
-						console.log(num);
+					console.log(num);
 					$this.animateNumber(
-					  {
-					    number: num,
-					    numberStep: comma_separator_number_step
-					  }, 7000
+						{
+							number: num,
+							numberStep: comma_separator_number_step
+						}, 7000
 					);
 				});
-				
+
 			}
 
-		} , { offset: '95%' } );
+		}, { offset: '95%' });
 
 	}
 	counter();
 
 
-	var contentWayPoint = function() {
+	var contentWayPoint = function () {
 		var i = 0;
-		$('.ftco-animate').waypoint( function( direction ) {
+		$('.ftco-animate').waypoint(function (direction) {
 
-			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
-				
+			if (direction === 'down' && !$(this.element).hasClass('ftco-animated')) {
+
 				i++;
 
 				$(this.element).addClass('item-animate');
-				setTimeout(function(){
+				setTimeout(function () {
 
-					$('body .ftco-animate.item-animate').each(function(k){
+					$('body .ftco-animate.item-animate').each(function (k) {
 						var el = $(this);
-						setTimeout( function () {
+						setTimeout(function () {
 							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
+							if (effect === 'fadeIn') {
 								el.addClass('fadeIn ftco-animated');
-							} else if ( effect === 'fadeInLeft') {
+							} else if (effect === 'fadeInLeft') {
 								el.addClass('fadeInLeft ftco-animated');
-							} else if ( effect === 'fadeInRight') {
+							} else if (effect === 'fadeInRight') {
 								el.addClass('fadeInRight ftco-animated');
 							} else {
 								el.addClass('fadeInUp ftco-animated');
 							}
 							el.removeClass('item-animate');
-						},  k * 50, 'easeInOutExpo' );
+						}, k * 50, 'easeInOutExpo');
 					});
-					
+
 				}, 100);
-				
+
 			}
 
-		} , { offset: '95%' } );
+		}, { offset: '95%' });
 	};
 	contentWayPoint();
 
 
 	// navigation
-	var OnePageNav = function() {
-		$(".smoothscroll[href^='#'], #ftco-nav ul li a[href^='#']").on('click', function(e) {
-		 	e.preventDefault();
+	var OnePageNav = function () {
+		$(".smoothscroll[href^='#'], #ftco-nav ul li a[href^='#']").on('click', function (e) {
+			e.preventDefault();
 
-		 	var hash = this.hash,
-		 			navToggler = $('.navbar-toggler');
-		 	$('html, body').animate({
-		    scrollTop: $(hash).offset().top
-		  }, 700, 'easeInOutExpo', function(){
-		    window.location.hash = hash;
-		  });
+			var hash = this.hash,
+				navToggler = $('.navbar-toggler');
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 700, 'easeInOutExpo', function () {
+				window.location.hash = hash;
+			});
 
 
-		  if ( navToggler.is(':visible') ) {
-		  	navToggler.click();
-		  }
+			if (navToggler.is(':visible')) {
+				navToggler.click();
+			}
 		});
 		$('body').on('activate.bs.scrollspy', function () {
-		  console.log('nice');
+			console.log('nice');
 		})
 	};
 	OnePageNav();
@@ -284,39 +284,39 @@
 
 	// magnific popup
 	$('.image-popup').magnificPopup({
-    type: 'image',
-    closeOnContentClick: true,
-    closeBtnInside: false,
-    fixedContentPos: true,
-    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-     gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-    },
-    image: {
-      verticalFit: true
-    },
-    zoom: {
-      enabled: true,
-      duration: 300 // don't foget to change the duration also in CSS
-    }
-  });
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
 
-  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
+	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
 
-    fixedContentPos: false
-  });
+		fixedContentPos: false
+	});
 
 
 	$('#book_pick_date,#book_off_date').datepicker({
-	  'format': 'm/d/yyyy',
-	  'autoclose': true
+		'format': 'm/d/yyyy',
+		'autoclose': true
 	});
 	$('#time_pick').timepicker();
 
@@ -324,3 +324,122 @@
 
 })(jQuery);
 
+// === ЛОГИКА ФОРМЫ БРОНИРОВАНИЯ И КАТАЛОГА ===
+$(document).ready(function () {
+
+	// 1. Активируем всплывающий календарь
+	if ($('.date-picker').length) {
+		$('.date-picker').datepicker({
+			format: 'mm/dd/yyyy',
+			autoclose: true,
+			todayHighlight: true,
+			startDate: new Date()
+		});
+	}
+
+	// 2. Восстанавливаем данные, если клиент вернулся из каталога
+	if (sessionStorage.getItem('bookingData')) {
+		const savedData = JSON.parse(sessionStorage.getItem('bookingData'));
+		$('#client-name').val(savedData.name);
+		$('#client-phone').val(savedData.phone);
+		$('#client-email').val(savedData.email);
+		$('#pickup-loc').val(savedData.pickupLoc);
+		$('#dropoff-loc').val(savedData.dropoffLoc);
+		$('#pickup-date').val(savedData.pickupDate);
+		$('#dropoff-date').val(savedData.dropoffDate);
+	}
+
+	// 3. Отображаем выбранный автомобиль (если он есть)
+	const selectedCar = sessionStorage.getItem('selectedCar');
+	if (selectedCar) {
+		$('#selected-car-label').text('Выбран: ' + selectedCar).css('color', '#f1b8c4');
+		$('#btn-select-car').text('Изменить автомобиль');
+	}
+
+	// 4. Переход в каталог (Сохраняем данные перед уходом)
+	$('#btn-select-car').on('click', function () {
+		const bookingData = {
+			name: $('#client-name').val(),
+			phone: $('#client-phone').val(),
+			email: $('#client-email').val(),
+			pickupLoc: $('#pickup-loc').val(),
+			dropoffLoc: $('#dropoff-loc').val(),
+			pickupDate: $('#pickup-date').val(),
+			dropoffDate: $('#dropoff-date').val()
+		};
+		sessionStorage.setItem('bookingData', JSON.stringify(bookingData));
+		window.location.href = 'car.html';
+	});
+
+	// 5. Отправка заявки с РЕАЛЬНЫМИ данными
+	$('#booking-form').on('submit', function (e) {
+		e.preventDefault();
+
+		// Проверка: выбрал ли клиент машину
+		if (!sessionStorage.getItem('selectedCar')) {
+			alert("Пожалуйста, сначала выберите автомобиль из каталога!");
+			return;
+		}
+
+		const submitBtn = $('#submit-btn');
+		const originalBtnText = submitBtn.val();
+		submitBtn.val('Отправка...').prop('disabled', true);
+
+		const pickupDateStr = $('#pickup-date').val();
+		const dropoffDateStr = $('#dropoff-date').val();
+		let rentalDays = 1;
+
+		if (pickupDateStr && dropoffDateStr) {
+			const start = new Date(pickupDateStr);
+			const end = new Date(dropoffDateStr);
+			const diffTime = Math.abs(end - start);
+			const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+			rentalDays = diffDays > 0 ? diffDays : 1;
+		}
+
+		// Собираем РЕАЛЬНЫЕ данные из полей ввода
+		const templateParams = {
+			subject_type: "ЗАЯВКА НА АРЕНДУ",
+			mode_label: "Аренда авто",
+			name: $('#client-name').val(),
+			phone: $('#client-phone').val(),
+			email: $('#client-email').val(),
+			pickup_date: pickupDateStr,
+			return_date: dropoffDateStr,
+			rental_days: rentalDays,
+			route_details: `Подача: ${$('#pickup-loc').val()} | Возврат: ${$('#dropoff-loc').val()}`,
+			tariff: sessionStorage.getItem('selectedCar'),
+			price: "Рассчитать с менеджером"
+		};
+
+		// ВНИМАНИЕ: Проверь первый аргумент, это должен быть Service ID (начинается с service_)
+		emailjs.send('template_oraxls7', 'template_j6uib56', templateParams)
+			.then(function () {
+				submitBtn.val('Успешно!').removeClass('btn-secondary').addClass('btn-success');
+				sessionStorage.removeItem('bookingData');
+				sessionStorage.removeItem('selectedCar');
+				$('#booking-form')[0].reset();
+				$('#selected-car-label').text('Автомобиль не выбран').css('color', '#fff');
+				$('#btn-select-car').text('Выбрать автомобиль');
+
+				setTimeout(() => { submitBtn.val(originalBtnText).removeClass('btn-success').addClass('btn-secondary').prop('disabled', false); }, 3000);
+			}, function (error) {
+				console.log(error);
+				submitBtn.val('Ошибка').removeClass('btn-secondary').addClass('btn-danger');
+				setTimeout(() => { submitBtn.val(originalBtnText).removeClass('btn-danger').addClass('btn-secondary').prop('disabled', false); }, 3000);
+			});
+	});
+});
+
+// === ЛОГИКА ВЫБОРА МАШИНЫ В КАТАЛОГЕ ===
+$(document).ready(function () {
+	$('.btn-book-car').on('click', function (e) {
+		e.preventDefault();
+		let carName = $(this).closest('.car-wrap').find('h2 a').text();
+		if (!carName) {
+			carName = $(this).closest('.car-wrap').find('h2').text();
+		}
+		sessionStorage.setItem('selectedCar', carName);
+		window.location.href = 'index.html';
+	});
+});
